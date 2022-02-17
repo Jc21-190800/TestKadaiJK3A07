3357838ec4931b92b754dcf0891d1cdd485d7659
慶悟 利部 <190800@jc-21.jp>
2022-02-17 17:48:53 +0900

    プロジェクト ファイルを追加します。

---
+++ kuriage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sisukaiyosizawa
+{
+    public class kuriage
+    {
+        public static int kuriage10(int i)
+        {
+            if (i == 20)
+            {
+                return 20;
+            }
+            else
+            {
+                return 10;
+            }
+        }
+    }
+}
